d5fef69b627432a9aab9bf7a23d65d480764a378
3bouda <abdelaliiheb@gmail.com>
2020-01-29 10:56:37 +0100

    the add works fine

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { FormsModule,ReactiveFormsModule} from '@angular/forms'
+import { FormsModule,ReactiveFormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { QuestionComponent } from './question/question.component';
-import { SectionComponent } from './section/section.component';
 import { QuestionnaireComponent } from './questionnaire/questionnaire.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuestionComponent,
-    SectionComponent,
     QuestionnaireComponent
   ],
   imports: [
--- src/app/questionnaire/questionnaire.component.ts
+++ src/app/questionnaire/questionnaire.component.ts
@@ -1,15 +1,68 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup, FormArray } from '@angular/forms'
 @Component({
   selector: 'app-questionnaire',
   templateUrl: './questionnaire.component.html',
   styleUrls: ['./questionnaire.component.css']
 })
 export class QuestionnaireComponent implements OnInit {
-
-  constructor() { }
+questionnaire:FormGroup;
+question:FormArray;
+answer:FormArray;
+  constructor(private fb:FormBuilder) { }
 
   ngOnInit() {
+    this.questionnaire=this.fb.group({ 
+      questionnaireName: [''],      
+      section : this.fb.array([this.creatSection()])
+    })
   }
 
+
+creatSection():FormGroup{
+return this.fb.group({
+ sectionName:[''],
+ question:this.fb.array([this.creatQuestion()])
+}) 
+}
+public get section(){
+  return this.questionnaire.controls['section'] as FormArray;
+}
+addSection(){
+
+  this.section.push(this.creatSection());
+}
+
+creatQuestion():FormGroup{
+  return this.fb.group({
+    questionName:[''],
+    type:['checkbox'],
+    answer:this.fb.array([this.creatAnswer()])
+  })
+}
+
+addQuestion(groupSection:FormArray){
+  const arrayQuestion=groupSection.get('question') as FormArray
+  arrayQuestion.push(this.creatQuestion())
+}
+
+creatAnswer():FormGroup{
+  return this.fb.group({
+    id:[''],
+    answerName:['']
+  })
+}
+
+addAnswer(groupQuestion:FormArray){
+  const arrayAnswer=groupQuestion.get('answer') as FormArray;
+arrayAnswer.push(this.creatAnswer());
+}
+
+onSubmit(){
+  console.log(this.questionnaire.value); 
+}
+// removeanswer(i){
+   
+//   this.lesreponses.removeAt(i);
+// }
 }
